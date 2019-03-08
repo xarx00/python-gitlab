b691b73ba63b4956efa57b9ff515400263bd432e
xarx00 <xarx@sms.cz>
2019-03-08 14:22:20 +0100

    Work in progress

--- gitlab/v4/tree_obj.py
+++ gitlab/v4/tree_obj.py
@@ -11,49 +11,43 @@ from gitlab import types
 from gitlab import utils
 
 
-class Tree(SaveMixin, ObjectDeleteMixin, RESTObject):
-    @cli.register_custom_action('GeoNode')
-    @exc.on_http_error(exc.GitlabRepairError)
-    def repair(self, **kwargs):
-        """Repair the OAuth authentication of the geo node.
-
-        Args:
-            **kwargs: Extra options to send to the server (e.g. sudo)
-
-        Raises:
-            GitlabAuthenticationError: If authentication is not correct
-            GitlabRepairError: If the server failed to perform the request
-        """
-        path = '/geo_nodes/%s/repair' % self.get_id()
-        server_data = self.manager.gitlab.http_post(path, **kwargs)
-        self._update_attrs(server_data)
-
-    @cli.register_custom_action('GeoNode')
-    @exc.on_http_error(exc.GitlabGetError)
-    def status(self, **kwargs):
-        """Get the status of the geo node.
-
-        Args:
-            **kwargs: Extra options to send to the server (e.g. sudo)
-
-        Raises:
-            GitlabAuthenticationError: If authentication is not correct
-            GitlabGetError: If the server failed to perform the request
-
-        Returns:
-            dict: The status of the geo node
-        """
-        path = '/geo_nodes/%s/status' % self.get_id()
-        return self.manager.gitlab.http_get(path, **kwargs)
+class Tree(RESTObject):
+    _id_attr = None
 
 
-class TreeManager(RetrieveMixin, UpdateMixin, DeleteMixin, RESTManager):
-    _path = '/geo_nodes'
+class TreeManager(ListMixin, RESTManager):
+    """Manager for work with the groups and projects hierarchy.
+
+    This manager doesn't actually manage objects but provides helper functions
+    for bulk operations over projects under a specified group.
+    """
+    _path = ''
     _obj_cls = Tree
-    _update_attrs = (tuple(), ('enabled', 'url', 'files_max_capacity',
-                               'repos_max_capacity'))
 
-    @cli.register_custom_action('TreeManager')
+    @cli.register_custom_action('TreeManager', ('full-path', ))
+    @exc.on_http_error(exc.GitlabGetError)
+    def group(self, **kwargs):
+        gl = self.gitlab
+
+        grpath = kwargs['full-path']
+        grname = grpath[grpath.rfind('/')+1:]
+        groups = gl.groups.list(search=grname, all-available=True)
+        for group in groups:
+            if group.full_path == grpath:
+                return group
+        raise exc.GitlabGetError(404)
+
+
+        print(groups)
+        groups = groups[1].subgroups.list()
+        print(groups)
+        grp = groups[5]
+        print(grp)
+        groups = grp.subgroups.list()
+        print(groups)
+        
+
+    @cli.register_custom_action('TreeManager', ('full-path', ))
     @exc.on_http_error(exc.GitlabGetError)
     def status(self, **kwargs):
         """Get the status of all the geo nodes.
@@ -68,21 +62,6 @@ class TreeManager(RetrieveMixin, UpdateMixin, DeleteMixin, RESTManager):
         Returns:
             list: The status of all the geo nodes
         """
-        return self.gitlab.http_list('/geo_nodes/status', **kwargs)
+#        return self.gitlab.http_list('/geo_nodes/status', **kwargs)
+        return self.gitlab.groups.list()
 
-    @cli.register_custom_action('TreeManager')
-    @exc.on_http_error(exc.GitlabGetError)
-    def current_failures(self, **kwargs):
-        """Get the list of failures on the current geo node.
-
-        Args:
-            **kwargs: Extra options to send to the server (e.g. sudo)
-
-        Raises:
-            GitlabAuthenticationError: If authentication is not correct
-            GitlabGetError: If the server failed to perform the request
-
-        Returns:
-            list: The list of failures
-        """
-        return self.gitlab.http_list('/geo_nodes/current/failures', **kwargs)
